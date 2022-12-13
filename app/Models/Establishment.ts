e7326295436c2956b8cef02377c28ef81e21f3e4
marinagosson <marina.gosson@gmail.com>
2022-12-13 16:10:53 -0300

    created admin, client and establishment models

---
+++ app/Models/Establishment.ts
@@ -0,0 +1,26 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Establishment extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public userId: number
+
+  @column()
+  public name: string
+
+  @column()
+  public logo: string | null
+
+  @column()
+  public online: boolean
+
+  @column()
+  public blocked: boolean
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+}
