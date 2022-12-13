9dc12030fd36be51993d161a91ee1b5a4a1d07fe
marinagosson <marina.gosson@gmail.com>
2022-12-13 15:21:34 -0300

    creation of iniital routes

--- start/routes.ts
+++ start/routes.ts
@@ -18,13 +18,43 @@
 |
 */
 
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Route from '@ioc:Adonis/Core/Route'
+import Database from '@ioc:Adonis/Lucid/Database';
 import User from 'App/Models/User'
 
 Route.get('/', async () => {
-  User.create({
-    email: 'marina.gosson@gmail.com',
-    password: '123456',
-    type: 'admin'
-  })
+  return 'welcome';
 })
+
+Route.post('/users',async ({request, response}: HttpContextContract) => {
+  const email = request.input('email')
+  const password = request.input('password')
+  if(email == null) return response.badRequest({"success": false, "message": "Campo e-mail não informado", "data": null})
+  if(password == null) return response.badRequest({"success": false, "message": "Campo password não informado", "data": null})
+  const createdUser = await User.create({
+    email: email,
+    password: password
+  })
+  return response.ok({"success": true, "message": "Usuário criado com sucesso", "data": createdUser.toJSON()})
+})
+
+Route.post('/auth', async ({request, response, auth}: HttpContextContract) => {
+
+  const email = request.input('email')
+  const password = request.input('password')
+  const user = await User.findBy('email', email);
+  
+  if(user == null) {
+    return response.notFound({"success": false, "message": "Usuário não encontrado", "data": null})
+  } else {
+    const token = await auth.use('api').attempt(email, password);
+    return response.ok(token)
+  }
+})
+
+Route.get('/users', async () => {
+  const users = await Database.from('users').select('*')
+  return users;
+  
+}).middleware('auth')
