4fe523ef7a1ad1d142e87725ec59b9eef7fcaf20
marinagosson <marina.gosson@gmail.com>
2023-01-13 17:23:59 -0300

    created auth controller

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,38 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+
+export default class AuthController {
+    public async login({auth, request, response}: HttpContextContract){
+        const email = request.input('email')
+        const password = request.input('password')
+
+        try {
+            const user = await User.findByOrFail('email', email);
+            let expired;
+
+            switch(user.type) {
+                case 'clients':
+                    expired = '30days';
+                    break;
+                case 'establishments':
+                    expired = '7days';
+                    break;
+                case 'admins':
+                    expired = '1day';
+                    break;
+                default:
+                    expired = '30days';
+                    break;
+            }
+
+            const token = await auth.use("api").attempt(email, password, {
+                expiresIn: expired,
+                name: user?.serialize().email()
+            })
+
+            response.ok(token);
+        } catch {
+            return response.badRequest('Invalid credencials')
+        }
+    }
+}
